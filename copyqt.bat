48b5b56efd96d9f9faa3c96814cb55b7fb998680
soramimi <fi7s-fct@asahi-net.or.jp>
2015-12-04 12:17:51 +0900

    batch files

---
+++ copyqt.bat
@@ -0,0 +1,3 @@
+copy C:\Qt\Qt5.5.1\5.5\msvc2013\bin\Qt5Core.dll build\release
+copy C:\Qt\Qt5.5.1\5.5\msvc2013\bin\Qt5Gui.dll build\release
+copy C:\Qt\Qt5.5.1\5.5\msvc2013\bin\Qt5Widgets.dll build\release
---
+++ regfree.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Bin\mt.exe" -manifest MyApp.exe.manifest -outputresource:build\release\MyApp.exe;1 
